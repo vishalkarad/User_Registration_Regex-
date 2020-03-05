0b460afd361ff3fac80e94bfa82d28133bebf752
Your Name <you@example.com>
2020-03-05 15:22:04 +0530

    Added : last name is valid or not

--- src/test/java/UserRegistrationTestCaces.java
+++ src/test/java/UserRegistrationTestCaces.java
@@ -16,4 +16,16 @@ public class UserRegistrationTestCaces {
         boolean firstName=obj.nameValidation("vishal");
         Assert.assertFalse(firstName);
     }
+    // Check to last name is valid
+    @Test
+    public void givenLastName_whenValid_thenReturn() {
+        boolean lastName=obj.nameValidation("Karad");
+        Assert.assertTrue(lastName);
+    }
+    // Check to last name is not valid
+    @Test
+    public void givenLastname_whenInvalid_thenReturn() {
+        boolean lastName=obj.nameValidation("kard");
+        Assert.assertFalse(lastName);
+    }
 }
